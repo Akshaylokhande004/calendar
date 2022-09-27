a398cff64710321df9394cb597b5b0de724497d6
Akshay <akshaylokhande044@gmail.com>
2022-09-27 15:06:42 +0530

    attendance

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+// import 'package:calender_check/example.dart';
+import 'package:flutter/material.dart';
+
+import 'example.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: new ThemeData(
+      primaryColor: const Color(0xFF02BB9F),
+      primaryColorDark: const Color(0xFF167F67),
+      accentColor: const Color(0xFF167F67),
+    ),
+    home: SafeArea(
+      child: CalendarPage2(),
+    ),
+  ));
+}
